4c4db78d3c800c80e035e852ec11cbde3a31d36a
jrevote <jerico.revote@gmail.com>
2013-11-15 17:19:55 +1100

    Updated workworkshop class, added helper defined resources for pulling required NGS datasets from object storage.

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,10 +35,90 @@
 #
 # Copyright 2013 Your name here, unless otherwise noted.
 #
-class workshop {
+class workshop($parent_dir='/mnt/NGS_workshop', $data_dir='data', 
+               $working_dir='working_dir', $trainee_user='ngstrainee') {
 
-  package { 'ipython':
-    ensure => installed,
+  # NGS Trainee User
+  $trainee_uid = 1001
+
+  File {
+    owner => $trainee_user,
+    group => $trainee_user,
+  }
+
+  group { $trainee_user:,
+    ensure => present,
+  }
+
+  file { "/home/${trainee_user}":
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  file { "/home/${trainee_user}/Desktop":
+    ensure  => directory,
+    mode    => '0755',
+    require => File["/home/${trainee_user}"],
+  }
+
+  user { $trainee_user:
+    ensure  => present,
+    uid     => $trainee_uid,
+    gid     => $trainee_user,
+    shell   => '/bin/bash',
+    home    => "/home/${trainee_user}",
+    require => [Group[$trainee_user], File["/home/${trainee_user}"]],
+  }
+
+  # Parent Directory
+  file { $parent_dir:
+    ensure  => directory,
+    mode    => '0755',
+    require => User[$trainee_user], 
+  }
+
+  # Data Directory
+  file { "${parent_dir}/${data_dir}":
+    ensure  => directory,
+    mode    => '0755',
+    require => File[$parent_dir],
+  }
+
+  # Work Directory
+  file { "${parent_dir}/${working_dir}":
+    ensure  => directory,
+    mode    => '0755',
+    require => File[$parent_dir],
+  }
+
+  define remote_file($remote_location, $mode=0644, $owner, $group) {
+    exec { "get_${title}":
+      command => "/usr/bin/wget -q ${remote_location} -O ${workshop::parent_dir}/${workshop::data_dir}/${title}",
+      creates => "${workshop::parent_dir}/${workshop::data_dir}/${title}",
+    }
+   
+    file { "${workshop::parent_dir}/${workshop::data_dir}/${title}":
+      mode    => $mode,
+      owner   => $owner,
+      group   => $group,
+      require => Exec["get_${title}"],
+    }
+  }
+
+  define workshop_file($location, $link) {
+    remote_file { $title:
+      remote_location => "${location}/${title}",
+      mode            => '0644',
+      owner           => $workshop::trainee_user,
+      group           => $workshop::trainee_user,
+    }
+    
+    file { "${link}/${title}":
+      ensure => link,
+      target => "${workshop::parent_dir}/${workshop::data_dir}/${title}",
+      owner  => $workshop::trainee_user,
+      group  => $workshop::trainee_user,
+    }
   }
 
 }
